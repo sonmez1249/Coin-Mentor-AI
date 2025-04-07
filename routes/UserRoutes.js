6bd4f4aff98b7574bf9531c929526e32e2dce934
MUHAMMED SÖNMEZ <120255174+sonmez1249@users.noreply.github.com>
2025-04-07 09:31:17 +0300

    update 2

---
+++ routes/UserRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Kullanıcı oluşturma (POST /api/users)
+router.post('/users', async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    const newUser = new User({ name, email });
+    await newUser.save();
+
+    res.status(201).json({ message: 'Kullanıcı başarıyla oluşturuldu!', user: newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Bir hata oluştu.' });
+  }
+});
+
+module.exports = router;
